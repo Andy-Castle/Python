7961a53b8fcdea1688cc49d3ee2e69163eb26d25
Andy-Castle <masterchief_and@live.com>
2025-05-01 21:22:50 -0500

    Arithmetic Operators

--- Operators.py
+++ Operators.py
@@ -1,3 +1,19 @@
 number = 25 #assign operator
 
+#Arithmetic Operators
+1 + -1 #0
+1 + 1 #2
+2 - 1 #1
+2 * 2 #4
+4 / 2 #2
+4 % 3 #1
+4 ** 2 #16
+5 // 2 #2 #floor division redondea el resultado hacia abajo al entero mas cercano
+
+print("Manchas" + " is a good dog")
+
+age = 8
+# age +=8 # age = age + 8
+age *=8 # age = age * 8
+print(age)
 
