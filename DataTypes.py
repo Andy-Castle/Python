2161d0e434a720c55e132aa24c6eedbadd7f5f94
Andy-Castle <masterchief_and@live.com>
2025-05-01 21:15:28 -0500

    Data types

---
+++ DataTypes.py
@@ -0,0 +1,20 @@
+# name = "Andy"
+# print(type(name) == str)
+# print(isinstance(name, str))
+
+# age = float(2)
+number = "20"
+age = int (number)
+# print(isinstance(age, float))
+print(isinstance(age, int))
+
+
+#complex for complex numbers
+#bool for booleans
+#list for lists
+#tuple for tuples
+#range for ranges
+#dict for dictionaries
+#set for sets
+
+
