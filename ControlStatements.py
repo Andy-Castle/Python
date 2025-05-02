a1586db0937eb125c03d10def0be45d703fe8769
Andy-Castle <masterchief_and@live.com>
2025-05-02 16:11:36 -0500

    Control Statements, declaraciones de control para evaluar condiciones y saludar a usuarios específicos

---
+++ ControlStatements.py
@@ -0,0 +1,15 @@
+condition = False
+name = "Flavio"
+
+if condition == True:
+  print("The condition")
+  print("was true")
+elif name == "Roger":
+  print("Hello Roger")
+elif name == "Syd":
+  print("Hello Syd")
+elif name == "Flavio":
+  print("Hello Flavio")
+else:
+  print("The condition")
+  print("was False")
