036a45dcd02d84d3466173a0f99d70c237e1fd2d
Andy-Castle <masterchief_and@live.com>
2025-05-04 18:26:49 -0500

    Estos son las anotaciones es como los tipos pero en si no son los tipos de datos que espera y el tipo de dato de salida que espera

---
+++ Annotations.py
@@ -0,0 +1,11 @@
+# Annotations
+
+"""
+Estos son las anotaciones es como los tipos pero en si no
+son los tipos de datos que espera y el tipo de dato de 
+salida que espera
+"""
+def increment(n: int) -> int:
+  return n + 1
+
+count: int = 0
